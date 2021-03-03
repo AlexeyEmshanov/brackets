24579f7d598cb5834e687d304eb88431a04f1d1e
AlexeyEmshanov <alexey.emshanov@gmail.com>
2021-03-03 23:06:46 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const configStr = bracketsConfig.map(elem => elem[0] + elem[1]) 
+  
+  while (str !== "") {
+    let isMatched = false
+    configStr.forEach(element => {
+      if (str.includes(element)) {
+        str = str.replace(element, "") 
+        isMatched = true
+      } 
+    })
+    if (!isMatched) {
+      break
+    }
+  }
+  
+  return (str !== "") ? false : true
 }
